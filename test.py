d2ae40565197806d19fbb63a8f886fc3be79db20
Feroz Arshad <ferozarshad99@gmail.com>
2025-05-02 12:28:04 +0500

    list branch removed

--- test.py
+++ test.py
@@ -1,8 +1,29 @@
-enc = "utf-8"
-a = open(r"C:\Users\feroz\OneDrive\Desktop\a.txt" , "a" , encoding=enc)
-a.write('Hello New Line\n')
-a.close()
+# enc = "utf-8"
+# a = open(r"C:\Users\feroz\OneDrive\Desktop\a.txt" , "a" , encoding=enc)
+# a.write('Hello New Line\n')
+# a.close()
 
-b = open(r"C:\Users\feroz\OneDrive\Desktop\a.txt" , "r" , encoding=enc)
-c = b.readlines()
-print(*c , sep="\n")
+# b = open(r"C:\Users\feroz\OneDrive\Desktop\a.txt" , "r" , encoding=enc)
+# c = b.readlines()
+# print(*c , sep="\n")
+
+# list = [19, 19, 15, 0, 5, 5, 1, 2,5]
+# def check_number(list):
+#     status = False
+#     fifth_element = list[4]
+#     print(f"Fifth Element is: {fifth_element}")
+#     count = 0
+#     for x in range(len(list)):
+#         if list[x] == fifth_element:
+#             count+=1
+#     if count == 3:
+#         status = True
+#     else:
+#         return status
+#     return status
+
+
+# a= check_number(list)
+# print(a)
+
+
